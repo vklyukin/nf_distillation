6e8536ddfe46f299c83ebd993f5ca19845aa23e6
Valeriy Klyukin <mr.clyukin@gmail.com>
2021-08-22 11:47:55 +0300

    make executable

--- run_cluster_grid.py
+++ run_cluster_grid.py
@@ -1,21 +1,28 @@
+#! /usr/bin/env python3
+
+import os
 import subprocess
 import time
 
 
 # training teachers for now
 
+
 def main():
+    username = os.environ["USER"]
+
     processes = []
     commands = []
-    name2training = {'celeba': 'celeba',
-                     'cifar': 'cifar',
-                     'power': 'tabular',
-                     'gas': 'tabular',
-                     'hepmass': 'tabular',
-                     'miniboone': 'tabular',
-                     'bsds300': 'tabular',
-                     }
-    for name in ['celeba', 'cifar', 'power', 'gas', 'hepmass', 'miniboone', 'bsds300']:
+    name2training = {
+        "celeba": "celeba",
+        "cifar": "cifar",
+        "power": "tabular",
+        "gas": "tabular",
+        "hepmass": "tabular",
+        "miniboone": "tabular",
+        "bsds300": "tabular",
+    }
+    for name in ["celeba", "cifar", "power", "gas", "hepmass", "miniboone", "bsds300"]:
         for _ in range(3):
             commands.append(
                 f"sbatch -t 15000 --gpus=1 -p normal -c 4 run_cluster.sh "
@@ -26,18 +33,25 @@ def main():
     batch_size = 20
     for command in commands:
         print(command)
-        process = subprocess.Popen(command,
-                                   shell=True,
-                                   close_fds=True,
-                                   )
+        process = subprocess.Popen(
+            command,
+            shell=True,
+            close_fds=True,
+        )
         processes.append(process)
-        pr_count = subprocess.Popen("squeue | grep martemev | wc -l", shell=True, stdout=subprocess.PIPE)
+        pr_count = subprocess.Popen(
+            f"squeue | grep {username} | wc -l", shell=True, stdout=subprocess.PIPE
+        )
         out, err = pr_count.communicate()
         if int(out) > batch_size:
             while int(out) > batch_size:
                 print("Waiting... ")
                 time.sleep(240)
-                pr_count = subprocess.Popen("squeue | grep martemev | wc -l", shell=True, stdout=subprocess.PIPE)
+                pr_count = subprocess.Popen(
+                    f"squeue | grep {username} | wc -l",
+                    shell=True,
+                    stdout=subprocess.PIPE,
+                )
                 out, err = pr_count.communicate()
 
     for process in processes:
